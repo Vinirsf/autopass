c7ff1463aadec30def506e61c0a3be6b157ba9f4
Vinirsf <viniryansf@gmail.com>
2025-05-08 00:46:57 -0300

    teste

--- public/src/scripts/supabase.js
+++ public/src/scripts/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Pega variáveis de ambiente (no Netlify você configura essas variáveis!)
 const SUPABASE_URL = 'https://pgjcbjnlcmsjzabehthy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnamNiam5sY21zanphYmVodGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4Nzc4NjMsImV4cCI6MjA2MTQ1Mzg2M30.g8_Xm_r2dbUZlO4haMwDOgXO-e2JsVGncnWltP1diC4';
 
